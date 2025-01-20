02406cabcdb9bfeaeedf2cc39a15f7d63e4de440
Igor Monteiro <contato.igormonteiro@gmail.com>
2025-01-20 15:01:39 -0300

    refactor

---
+++ src/main/java/com/example/menu/user/AuthenticationDTO.java
@@ -0,0 +1,4 @@
+package com.example.menu.user;
+
+public record AuthenticationDTO(String login, String password) {
+}
---
+++ src/main/java/com/example/menu/user/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.menu.user;
+
+public record LoginResponseDTO(String token) {
+}
